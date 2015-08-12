2b3f0862a5cba576da64a532819005b153461188
Zach Aysan <zachaysan@gmail.com>
2015-08-12 19:25:27 -0400

    Add ssh manager, droplet manager, and deployer stubs

--- main.rb
+++ main.rb
@@ -1,4 +1,24 @@
+require 'droplet_kit'
+require 'pp'
+
 require_relative "secrets.rb"
+require_relative "ssh_manager.rb"
+require_relative "droplet_manager.rb"
+require_relative "deployer.rb"
 
 secrets = Secrets.new
-puts secrets.digital_ocean_key
+
+client = DropletKit::Client
+  .new(access_token: secrets.digital_ocean_key)
+
+ssh_manager = SSHManager.new(client)
+ssh_manager.add_pub_key_if_not_exists( secrets.ssh_pub_key )
+
+droplet_manager = DropletManager.new(client,
+                                     "testing",
+                                     ssh_manager.key)
+
+droplet_manager.set_droplet_target(2)
+
+deployer = Deployer.new(droplet_manager)
+deployer.instruct_all("date")
