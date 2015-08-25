117b36618520d4efacf277a15e343d4fa69e784f
Zach Aysan <zachaysan@gmail.com>
2015-08-25 11:57:05 -0400

    Horray I'm not a lazy developer that just lets blatant security holes through

--- deployer.rb
+++ deployer.rb
@@ -8,11 +8,7 @@ class Deployer
     @droplet_manager.droplets.each do |droplet|
       host = "root@#{droplet.networks.v4.first.ip_address}"
 
-      # StrictHostKeyChecking=no means that we trust the first
-      # SSH connection attempt, since we aren't going to check
-      # anyway, but if there is a *change* in the key, then we
-      # still want the command to fail.
-      ssh_command = "ssh -o StrictHostKeyChecking=no"
+      ssh_command = "ssh"
 
       puts `#{ssh_command} #{host} #{message}`
     end
