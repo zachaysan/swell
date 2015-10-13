826a6edaf35d813959bf2ede02fbbdd16102ed12
Zach Aysan <zachaysan@gmail.com>
2015-10-13 14:22:11 -0400

    +\n

--- lib/swell/known_host.rb
+++ lib/swell/known_host.rb
@@ -24,6 +24,7 @@ class KnownHost
                             ".ssh",
                             "known_hosts")
     line = self.to_s
+
     open(known_hosts, 'a') do |f|
       f.puts line
     end
